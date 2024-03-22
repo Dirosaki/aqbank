9b7645a2d41deddcb6e8ef79c502ee6674b93bb8
Dirosaki <diegosgomes27@gmail.com>
2024-03-22 15:22:26 -0300

    feat: Adding label component

---
+++ src/components/Form/Label.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps } from 'react'
+
+import { cn } from '@/utils/cn'
+
+type LabelProps = ComponentProps<'label'> & {
+  helper?: string
+}
+
+export function Label({ className, htmlFor, children, helper, ...props }: LabelProps) {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={cn('text-sm font-medium text-neutral-950 tracking-[0.5px]', className)}
+      {...props}
+    >
+      {children}
+      {helper && <span className="ml-1 font-normal text-zinc-400">({helper})</span>}
+    </label>
+  )
+}
