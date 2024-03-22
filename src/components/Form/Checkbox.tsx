6e69e5630b77b1834eaa6f79e75400f270526031
Dirosaki <diegosgomes27@gmail.com>
2024-03-22 15:23:06 -0300

    feat: Adding checkbox component

---
+++ src/components/Form/Checkbox.tsx
@@ -0,0 +1,25 @@
+import { ComponentProps, forwardRef } from 'react'
+
+import { Icon } from '@/components/Icon'
+import { cn } from '@/utils/cn'
+
+type CheckboxProps = ComponentProps<'input'>
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ className, ...props }, ref) => (
+    <div className={cn('relative flex items-center justify-center w-min', className)}>
+      <input
+        type="checkbox"
+        className="peer size-5 cursor-pointer appearance-none rounded border border-neutral-300 transition-colors checked:bg-neutral-950 hover:border-neutral-950 checked:hover:border-neutral-300 checked:hover:bg-neutral-900 focus-visible:outline-none focus-visible:ring-[1.5px] focus-visible:ring-neutral-950/50"
+        ref={ref}
+        {...props}
+      />
+      <Icon
+        name="check"
+        size={16}
+        className="pointer-events-none absolute hidden text-white peer-checked:block"
+      />
+    </div>
+  )
+)
+Checkbox.displayName = 'Checkbox'
